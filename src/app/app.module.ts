7a02bba13c4902d99dd95235c27ee7602a3173ec
rully saputra <rully.saputra4@gmail.com>
2019-11-25 23:36:18 +0700

    first version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -12,10 +13,11 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule],
   providers: [
     StatusBar,
     SplashScreen,
+    HttpClient,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+   res : any ;
+   public homeScore : any;
+   public awayScore : any;
+   public home : any;
+   public away : any;
+  constructor(private http:HttpClient) {}
+  onSubmit(){
+    //http://localhost:9000/?
+    this.http.get('http://192.168.0.112:5000/?' + 'home=' + this.home +'&away=' + this.away).subscribe(respon => {
 
-  constructor() {}
-
+      if(respon['prediksi'] === '[1]'){
+        this.res = 'Pertandingan berakhir seri!';
+      } else if(respon['prediksi'] === '[2]'){
+        this.res = 'Pertandingan dimenangkan oleh HomeTeam!';
+      } else {
+        this.res = 'Pertandingan dimenangkan oleh AwayTeam!';
+      }
+      this.homeScore = respon['Home'];this.awayScore = respon['Away'];
+    });
+  }
+  onChangeHome(event){
+    this.home = event.detail.value
+  }
+  onChangeAway(event){
+    this.away = event.detail.value
+  }
 }
